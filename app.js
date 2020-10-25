8a5d2d6c8879f4c814ea197c37849df586ec1aac
lopez-marco <lopez.marcohdez@gmail.com>
2020-10-24 23:06:00 -0400

    first commit to deployment

--- app.js
+++ app.js
@@ -42,4 +42,4 @@ app.use('/items', items);
 app.use('/categories', categories);
 app.use('/coupons', coupons)
 
-app.listen(3001, () => console.log("*** APP LISTENING ON PORT 3001 ***"));
+app.listen(3001, () => console.log(`*** APP LISTENING ON PORT 3001*** ${process.env.PORT}`));
